adedf981fbdce71ec61bcddbf41e108b2ff057cf
dariabenhardt <daria.benhardt@gmail.com>
2025-10-13 09:49:29 +0200

    Add

---
+++ src/main/java/de/example/likeherotozero/LikeherotozeroApplication.java
@@ -0,0 +1,13 @@
+package de.example.likeherotozero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeherotozeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeherotozeroApplication.class, args);
+	}
+
+}
