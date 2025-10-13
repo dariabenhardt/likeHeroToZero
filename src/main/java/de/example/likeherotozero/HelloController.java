aef2e94e492c3117abf8549026242c00fd21d1ad
dariabenhardt <daria.benhardt@gmail.com>
2025-10-13 10:05:54 +0200

    Initial Commit

--- src/main/java/de/example/likeherotozero/HelloController.java
+++ src/main/java/de/example/likeherotozero/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello! Der Spring Server läuft!";
+        return "Hello! Der Spring Server läuft!" ;
     }
 }
